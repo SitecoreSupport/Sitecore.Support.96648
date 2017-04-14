d752124558654d0314c118af0f6fa4d88fe45fa1
Ivan Sheyenko <ivs@sitecore.net>
2017-04-14 16:11:44 +0300

    Item resolving logic modified

--- src/Sitecore.Support.96648/ItemLanguageResolver.cs
+++ src/Sitecore.Support.96648/ItemLanguageResolver.cs
@@ -1,5 +1,9 @@
-﻿using Sitecore.Globalization;
+﻿using Sitecore.Common;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Globalization;
 using Sitecore.Pipelines.HttpRequest;
+using Sitecore.Sites;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +12,23 @@ using System.Threading.Tasks;
 
 namespace Sitecore.Support.Pipelines.HttpRequest
 {
-    public class ItemLanguageResolver : HttpRequestProcessor
+    public class ItemLanguageResolver : ItemResolver
     {
         public override void Process(HttpRequestArgs args)
         {
+
+            
+
             string[] sites= new string[]{ "shell","login","admin","service", "system_layouts", "modules_shell", "modules_website", "scheduler", "system", "publisher" };
             if (sites.Contains(Context.Site.Name))
             {
+                base.Process(args);
                 return;
+            }
+
+            using(new EnforceVersionPresenceDisabler())
+            {
+                base.Process(args);
             }
 
             if (Context.Item != null)
@@ -23,6 +36,15 @@ namespace Sitecore.Support.Pipelines.HttpRequest
                 if (Context.Item.Versions.GetVersions(false).Count() == 0)
                 {
                     Context.Language = Language.Parse(Context.Site.Language);
+                    if (!IsItemEnforceVersionPresenceEnabled(Context.Item))
+                    {
+                        Context.Item = Context.Database.GetItem(Context.Item.ID);
+                    }
+                    else
+                    {
+                        Context.Item = null;
+                    }                  
+                   
                 }
             }
             else
@@ -30,5 +52,22 @@ namespace Sitecore.Support.Pipelines.HttpRequest
                 Context.Language = Language.Parse(Context.Site.Language);
             }
         }
+
+        protected virtual bool IsEnforceVersionPresenceEnabled()
+        {
+            SiteContext site= Context.Site;
+            return !Switcher<bool, EnforceVersionPresenceDisabler>.CurrentValue && site != null && site.SiteInfo.EnforceVersionPresence;
+        }
+
+        protected virtual bool IsItemEnforceVersionPresenceEnabled(Item item)
+        {
+            bool result;
+            using (new EnforceVersionPresenceDisabler())
+            {
+                result = (item != null && item.RuntimeSettings.TemporaryVersion && !item.Name.StartsWith("__") && item[FieldIDs.EnforceVersionPresence] == "1");
+            }
+            return result;
+        }
+
     }
 }
